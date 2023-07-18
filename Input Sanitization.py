3fe923838eda7e2f2b60b76c437135af1d3733f8
T1 <126980610+tadash10@users.noreply.github.com>
2023-07-18 20:30:38 -0300

    Create Input Sanitization.py

---
+++ Input Sanitization.py
@@ -0,0 +1,6 @@
+def sanitize_inputs(api_url, api_key):
+    # Add your input sanitization logic here based on your requirements
+    sanitized_api_url = api_url.strip()
+    sanitized_api_key = api_key.strip()
+
+    return sanitized_api_url, sanitized_api_key
